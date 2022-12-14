40d85420654990877454cbf0ebb94ef37994fad0
k0ds <cathating@protonmail.com>
2022-12-13 20:17:02 -0500

    done

--- CollatzConjecture/CollatzConjecture.cpp
+++ CollatzConjecture/CollatzConjecture.cpp
@@ -1,8 +1,49 @@
 
 #include <iostream>
 
+bool IsOdd(int num);
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int number = 0;
+    std::cout << "Enter the number you want to use: ";
+    std::cin >> number;
+
+    for (int i = 0; i < 3;)
+    {
+        if (!IsOdd(number))
+        {
+            number = number / 2;
+        }
+        else
+        {
+            number = number * 3 + 1;
+        }
+
+        if (number == 1)
+        {
+            i++;
+        }
+
+        std::cout << number << "\n";
+
+    }
+}
+
+
+bool IsOdd(int num)
+{
+    if (num % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+
+
 }
 
